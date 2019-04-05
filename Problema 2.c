139e3bba9dfcc73d3821a9c1425daaff1e6fd20a
João Gabriel Antunes <jg.antunes.medeiros@gmail.com>
2019-04-05 15:21:20 -0300

    Solução completa do problema 2.

--- Problema 2.c
+++ Problema 2.c
@@ -7,10 +7,10 @@ float *recebe_notas(float *notas, int maximo){
     for(int i = 0; i < maximo; i++){
         if(*(notas+i) >= 6.0){
             *(notas+i) = 1;
-            printf("high grade\n");
+            //printf("high grade\n");
         } else{
             *(notas+i) = 0;
-            printf("low grade\n");
+            //printf("low grade\n");
         }
 
     }
@@ -37,22 +37,41 @@ int *conta_notas(float *apr, int maximo){
     return res;
 }
 
-int percent_aprov(){
+int percent_aprov(int *vect){
+    int *perc;
+    perc = malloc(sizeof(int));
+    for (int i = 0; i < 2; i++){
+        perc[i] = vect[i] * 10;
+        
+    }
+    vect[0] = perc[0];
+    vect[1] = perc[1];
+    if(perc[0] > 50){
+        return 1;
+    } else
+        return 0;
 }
 
 int main(){
-    float notas[MAX]={1.2, 2.3, 4.5, 6.7, 8.7, 6.7, 8.7, 1.2, 4.5, 6.7}, *apr;
+    float notas[MAX]={1.2, 7.3, 4.5, 6.7, 8.7, 6.7, 8.7, 1.2, 4.5, 6.7}, *apr;
     int *res;
-    //for(int i = 0; i <  MAX; i++)
-    //   scanf("%f", &notas[i]);
+    int percentage;
+    printf("Escreva as notas a seguir: ");
+    for(int i = 0; i <  MAX; i++)
+       scanf("%f", &notas[i]);
 
     apr = recebe_notas(&notas[0], MAX);
     res = conta_notas(&apr[0], MAX);
+    printf("%d aprovados e %d reprovados\n", res[0], res[1]);
 
-    for(int i = 0; i < MAX; i++){
-        printf("%0.0f ", *(apr+i));
-    }
-    printf("\n%d %d", res[0], res[1]);
+    percentage = percent_aprov(&res[0]);
+    printf("%d por cento aprovado e %d por cento reprovado.\n", res[0], res[1]);
+    if(percentage == 1)
+        printf("Mais da metade da turma passou.\n");
+    else
+        printf("Mais da metade da turma reprovou.\n");
+    
+    
 
     return 0;
 }
